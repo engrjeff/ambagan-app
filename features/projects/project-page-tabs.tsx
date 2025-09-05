0805112e3a8073b3ea9e24845ebaa491664d2564
engrjeff <engineerjeff15@gmail.com>
2025-09-05 16:28:28 +0800

    update: enhance tabs ui

--- features/projects/project-page-tabs.tsx
+++ features/projects/project-page-tabs.tsx
@@ -1,6 +1,12 @@
 'use client';
 
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import {
+  ChartSplineIcon,
+  PhilippinePesoIcon,
+  SettingsIcon,
+  UsersIcon,
+} from 'lucide-react';
 import { useParams, useRouter, useSearchParams } from 'next/navigation';
 import { ReactNode } from 'react';
 
@@ -8,18 +14,22 @@ const projectPages = [
   {
     id: 'insights',
     label: 'Insights',
+    Icon: ChartSplineIcon,
   },
   {
     id: 'contributors',
     label: 'Contributors',
+    Icon: UsersIcon,
   },
   {
     id: 'payment-tracking',
     label: 'Payment Tracking',
+    Icon: PhilippinePesoIcon,
   },
   {
     id: 'settings',
     label: 'Settings',
+    Icon: SettingsIcon,
   },
 ];
 
@@ -38,10 +48,14 @@ export function ProjectPageTabs({ children }: { children: ReactNode }) {
         router.push(`/projects/${params.projectId}?tab=${value}`)
       }
     >
-      <TabsList>
+      <TabsList className="text-foreground h-auto gap-2 rounded-none border-b bg-transparent px-0 py-1">
         {projectPages.map((page) => (
-          <TabsTrigger key={page.id} value={page.id}>
-            {page.label}
+          <TabsTrigger
+            key={page.id}
+            value={page.id}
+            className="hover:bg-accent dark:data-[state=active]:text-primary dark:data-[state=active]:border-none hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 dark:data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+          >
+            <page.Icon /> {page.label}
           </TabsTrigger>
         ))}
       </TabsList>
