a10bb9844610a1658649574d7636654a701143f1
engrjeff <engineerjeff15@gmail.com>
2025-09-05 21:30:58 +0800

    add 404 pages

---
+++ app/not-found.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@/components/ui/button';
+import { app } from '@/lib/config';
+import { ArrowRightIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+function GlobalNotFoundPage() {
+  return (
+    <div className="text-center h-[60vh] flex flex-col items-center justify-center gap-4">
+      <Image
+        unoptimized
+        src="/icons/logo.svg"
+        width={40}
+        height={40}
+        alt={app.title}
+      />
+      <h1 className="text-xl font-bold">Page Not Found</h1>
+      <Button asChild>
+        <Link href="/projects">
+          Go to Projects <ArrowRightIcon />
+        </Link>
+      </Button>
+    </div>
+  );
+}
+
+export default GlobalNotFoundPage;
